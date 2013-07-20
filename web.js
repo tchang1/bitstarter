9d110e1b29f2b85334e014283fa80d67d8f0478b
Tony Chang <ampedlemon@gmail.com>
2013-07-20 01:09:57 +0000

    synchronous file read

--- web.js
+++ web.js
@@ -2,14 +2,12 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var buffer=fs.readFile('index.html', function (err,data) {
-if (err) throw err;
-console.log(data);
-});
+var buffer=fs.readFileSync('index.html')
+
 
  
 app.get('/', function(request, response) {
-  response.send(buffer.toString('utf-8'))
+  response.send(buffer)
 });
 
 var port = process.env.PORT || 5000;
