8b89710d46e85eb4e0ed9e08e8cbd6303914632e
Tony Chang <ampedlemon@gmail.com>
2013-07-20 01:11:33 +0000

    buffer to string

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ var buffer=fs.readFileSync('index.html')
 
  
 app.get('/', function(request, response) {
-  response.send(buffer)
+  response.send(buffer.toString('utf-8'))
 });
 
 var port = process.env.PORT || 5000;
