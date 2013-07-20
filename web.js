9d7d104b3fdb3d317e3443971493ec61eb94ce2c
Tony Chang <ampedlemon@gmail.com>
2013-07-20 01:04:38 +0000

    file location for index

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
-var buffer=fs.readFile('/index.html', function (err,data) {
+var buffer=fs.readFile('index.html', function (err,data) {
 if (err) throw err;
 console.log(data);
 });
